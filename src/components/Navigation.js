74a00420e9c9da732350b22fd6c4f3166a1003a7
Stéphane PIERRE <stephane_pierre@hotmail.fr>
2021-09-11 17:29:53 +0200

    Add navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,39 @@
 import React from "react";
 import { NavLink } from 'react-router-dom';
+import logo from '../media/logo.PNG';
+
 
 
 function Navigation() {
     return (
         <div className='navigation'>
+            <img src={logo} alt="Logo"/>;
+            <ul>
+                <li>
+                    <NavLink exact to="/" activeClassName="active">
+                        <span>Accueil</span>
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink exact to="/photosGallery" activeClassName="active">
+                        <span>Galerie</span>
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink exact to="/prices" activeClassName="active">
+                        <span>Tarifs et prestations</span>
+                    </NavLink>
+                </li>
+
+                <li>
+                    <NavLink exact to="/contact" activeClassName="active">
+                        <span>Contact</span>
+                    </NavLink>
+                </li>
+
+            </ul>
         </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import Navigation from "../components/Navigation";
 
 const home = () => {
     return (
-        <div className="home">
+        <>
             <Navigation/>
-            <div className='home-body'>
-                <h1>Charles Cantin - Photographe</h1>
-            </div>
-        </div>
+                <div className="home">
+                    <div className='home-body'>
+                        <h1>Charles Cantin - Photographe</h1>
+                    </div>
+                </div>
+        </>
     )
 }
 
